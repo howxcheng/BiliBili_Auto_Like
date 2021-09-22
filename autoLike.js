4e251da2cf47badd453c0ca854dd6b08d86fc963
程昊 <47664542+howxcheng@users.noreply.github.com>
2021-09-22 18:53:12 +0800

    update author info

--- autoLike.js
+++ autoLike.js
@@ -7,6 +7,10 @@
 // @author       Howxcheng
 // @include       *://*.bilibili.com/video/*
 // @include       *://*.bilibili.com/bangumi/*
+// @updateURL      https://raw.githubusercontent.com/howxcheng/BiliBili_Auto_Like/main/autoLike.js
+// @downloadURL    https://raw.githubusercontent.com/howxcheng/BiliBili_Auto_Like/main/autoLike.js
+// @homepageURL    https://github.com/howxcheng/BiliBili_Auto_Like
+// @supportURL      https://github.com/howxcheng/BiliBili_Auto_Like/issues
 // @icon         https://www.google.com/s2/favicons?domain=bilibili.com
 // @license        MIT
 // @compatible     chrome 80 or later
@@ -24,20 +28,6 @@
 // @connect        pushplus.plus
 // @connect        andywang.top
 // @connect        gitee.com
-// @require        https://cdn.jsdelivr.net/gh/andywang425/BLTH@dac0d115a45450e6d3f3e17acd4328ab581d0514/assets/js/library/Ajax-hook.min.js
-// @require        https://cdn.jsdelivr.net/npm/jquery@3.2.1/dist/jquery.min.js
-// @require        https://cdn.jsdelivr.net/gh/andywang425/BLTH@d810c0c54546b88addc612522c76ba481285298d/assets/js/library/decode.min.js
-// @require        https://cdn.jsdelivr.net/npm/pako@1.0.10/dist/pako.min.js
-// @require        https://cdn.jsdelivr.net/gh/andywang425/BLTH@f50572d570ced20496cc77fe6a0853a1deed3671/assets/js/library/bliveproxy.min.js
-// @require        https://cdn.jsdelivr.net/gh/andywang425/BLTH@91f469fef739c8ecfd4f101d3b4ba7e5e95be42d/assets/js/library/BilibiliAPI_Mod.min.js
-// @require        https://cdn.jsdelivr.net/gh/andywang425/BLTH@4368883c643af57c07117e43785cd28adcb0cb3e/assets/js/library/layer.min.js
-// @require        https://cdn.jsdelivr.net/gh/andywang425/BLTH@dac0d115a45450e6d3f3e17acd4328ab581d0514/assets/js/library/libBilibiliToken.min.js
-// @require        https://cdn.jsdelivr.net/gh/andywang425/BLTH@dac0d115a45450e6d3f3e17acd4328ab581d0514/assets/js/library/libWasmHash.min.js
-// @require        https://cdn.jsdelivr.net/npm/hotkeys-js@3.8.7/dist/hotkeys.min.js
-// @resource       layerCss https://cdn.jsdelivr.net/gh/andywang425/BLTH@f9a554a9ea739ccde68918ae71bfd17936bae252/assets/css/layer.css
-// @resource       myCss    https://cdn.jsdelivr.net/gh/andywang425/BLTH@5bcc31da7fb98eeae8443ff7aec06e882b9391a8/assets/css/myCss.min.css
-// @resource       main     https://cdn.jsdelivr.net/gh/andywang425/BLTH@f3caf1d878912d5d103bf1fa30e75f9cd20f144b/assets/html/main.min.html
-// @resource       eula     https://cdn.jsdelivr.net/gh/andywang425/BLTH@da3d8ce68cde57f3752fbf6cf071763c34341640/assets/html/eula.min.html
 // @grant          unsafeWindow
 // @grant          GM_xmlhttpRequest
 // @grant          GM_getResourceText
